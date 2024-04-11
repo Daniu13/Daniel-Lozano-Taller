810f165e1cd377d7e90c020b34b080bda4fd7f11
Daniu13 <daniel.lozano1@udea.edu.co>
2024-04-11 14:18:37 -0500

    Punto 1 a 3

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 """Daniel Lozano Simanca"""
+
+"""Punto 1"""
+aleatoria = np.random.randint(10, size=1200000).reshape((4,4,5,15000))
+print(aleatoria.shape)
+
+"""Punto 2 y 3"""
+copia = aleatoria[:,:,:,0].copy()
+print(f'\n\n{copia}')
+print(f'Shape: {copia.shape}')
+print(f'Size: {copia.size}')
