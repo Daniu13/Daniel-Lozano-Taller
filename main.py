b77dad32084c2e09e509caa6400e51e496265802
Daniu13 <daniel.lozano1@udea.edu.co>
2024-04-11 14:33:24 -0500

    Punto 5

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 """Daniel Lozano Simanca"""
 
 """Punto 1"""
-aleatoria = np.random.randint(10, size=1200000).reshape((4,4,5,15000))
+aleatoria = np.random.randint(10, size=1200000).reshape((2,8,5,15000))
 print(aleatoria.shape)
 
 """Punto 2 y 3"""
@@ -16,6 +16,13 @@ print(f'Size: {copia.size}')
 print(f'Dim: {copia.ndim}')
 
 """Punto 4"""
-copia2d = copia.reshape((2,8,5))[:,:,0].copy()
+copia2d = copia.reshape((4,4,5))[:,:,0].copy()
 print(copia2d)
 print(copia2d.shape)
+
+"""Punto 5"""
+def zu_pandas(array):
+   df = pd.DataFrame(array, columns=["c0", "c1", "c2", "c3"])
+   return df
+df = zu_pandas(copia2d)
+print(f'\n\n{df}')
